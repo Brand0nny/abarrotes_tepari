45ba348769067211ffb0a55ecfda73b9f52bf8b1
Brand0nny <brandonpenadmas@gmail.com>
2024-07-18 18:46:36 -0600

    first commit

---
+++ src/main/java/com/brand0nny/springboot/web/abarrotes_tepari/AbarrotesTepariApplication.java
@@ -0,0 +1,13 @@
+package com.brand0nny.springboot.web.abarrotes_tepari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbarrotesTepariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbarrotesTepariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brand0nny/springboot/web/abarrotes_tepari/AbarrotesTepariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brand0nny.springboot.web.abarrotes_tepari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbarrotesTepariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
